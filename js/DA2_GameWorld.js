627a25d5bc125d3fda509f25a54e35e8ae8f05fa
TheOnePercent44 <gamemakervx@gmail.com>
2015-02-10 12:29:45 -0500

    returned padding

--- js/DA2_GameWorld.js
+++ js/DA2_GameWorld.js
@@ -49,7 +49,7 @@ RPGGame.GameWorld.prototype = {
 		this.game.physics.enable(catfriend, Phaser.Physics.ARCADE);
 		catfriend.anchor.setTo(0.5, 0.5);
 		//catfriend.body.collideWorldBounds = true;
-		//catfriend.body.tilePadding.set(16, 16);
+		catfriend.body.tilePadding.set(16, 16);
 		//this.game.physics.enable(catfriend, Phaser.Physics.ARCADE);
 		//map.setCollision([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31], true, layer1, true);
 		//map.setCollisionByExclusion([0, 18], true, layer1);
